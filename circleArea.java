7a925a4d452c9627bc0c70272150f7ea303c3f2f
Utkarsh Singh <utkarshsingh102005@gmail.com>
2024-05-27 11:15:33 +0530

    Modified the output of area file

--- circleArea.java
+++ circleArea.java
@@ -6,7 +6,7 @@ public class circleArea {
         System.out.println("The radius of circle is ..");
         float radius = scn.nextFloat();
         float area = 3.14f*radius*radius;
-        System.out.println("Area of circle will be ..");
+        System.out.println("The area of circle will be ..");
         System.out.println(area);
         scn.close();
 
